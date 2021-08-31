fff9087d7148f12a226a3917e28be463e59e1f46
jeanarnaud ritouret <jeanarnaudritouret@gmail.com>
2021-08-31 15:32:24 +0200

    cluster api

--- api/clusterapi.py
+++ api/clusterapi.py
@@ -28,3 +28,12 @@ def predict(recipe_id='006ab0aafd', n_neighbors='5'):
     print(df_neighbors)
     return dict(prediction=df_neighbors)
     
+
+@app.get('/predict_similarities')
+def predict(recipe_id='006ab0aafd', n_similar='10'):
+    X_recipe_similarities = load_recipe_similarities()
+    X_similar_recipes = load_similar_recipes()
+    df_recipes_info = load_informational_data()
+    df_similar = get_similar_recipes(recipe_id, df_recipes_info, X_similar_recipes, X_recipe_similarities, n_similar)
+    print(df_similar)
+    return dict(prediction=df_similar)
